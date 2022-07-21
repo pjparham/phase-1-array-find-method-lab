f4ef75fcb0671499770d4fc3e2b5b42a298642a1
Parker Parham <parkerparham@gmail.com>
2022-07-21 13:42:06 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(collection){
+    for (const team of collection){
+        if (team.result === "W") {
+            return team.year;
+        }
+    }
+}
+
+
+// function superbowlWin(records){
+//     function isW(){
+
+//     }
+//     (records).find(isW);
+// }
